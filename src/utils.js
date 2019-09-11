0f47ee2dd05888528d3a4f8505c0d084b5aa3611
Чмелев Андрей Александрович <Chmelev_A@Dept07>
2019-09-11 15:10:13 +0300

    intersection are determined taking into account the rotation for center mode

--- src/utils.js
+++ src/utils.js
@@ -84,13 +84,7 @@ export function css(el, attr, val) {
 export function intersects(a, b, mode = 'touch') {
 
     if (mode === 'center') {
-        const bxc = b.left + b.width / 2;
-        const byc = b.top + b.height / 2;
-
-        return bxc >= a.left
-            && bxc <= a.right
-            && byc >= a.top
-            && byc <= a.bottom;
+        return doPolygonsIntersect(a, b);
     } else if (mode === 'cover') {
         return b.left >= a.left
             && b.top >= a.top
@@ -102,6 +96,85 @@ export function intersects(a, b, mode = 'touch') {
             && a.bottom >= b.top
             && a.top <= b.bottom;
     }
+}
+
+function isUndefined(a) {
+    return a === undefined;
+}
+
+/**
+ * Helper function to determine whether there is an intersection between the two polygons described
+ * by the lists of vertices. Uses the Separating Axis Theorem
+ *
+ * @param a an array of connected points [{x:, y:}, {x:, y:},...] that form a closed polygon
+ * @param b an array of connected points [{x:, y:}, {x:, y:},...] that form a closed polygon
+ * @return true if there is any intersection between the 2 polygons, false otherwise
+ */
+function doPolygonsIntersect (rectA, rectB) {
+    const a = mapRectToArrayCoordinate(rectA);
+    const b = mapRectToArrayCoordinate(rectB);
+    const polygons = [a, b];
+    let minA, maxA, projected, minB, maxB;
+
+    for (let i = 0; i < polygons.length; i++) {
+
+        // for each polygon, look at each edge of the polygon, and determine if it separates
+        // the two shapes
+        let polygon = polygons[i];
+        for (let i1 = 0; i1 < polygon.length; i1++) {
+
+            // grab 2 vertices to create an edge
+            let i2 = (i1 + 1) % polygon.length;
+            let p1 = polygon[i1];
+            let p2 = polygon[i2];
+
+            // find the line perpendicular to this edge
+            let normal = { x: p2.y - p1.y, y: p1.x - p2.x };
+
+            minA = maxA = undefined;
+            // for each vertex in the first shape, project it onto the line perpendicular to the edge
+            // and keep track of the min and max of these values
+            for (let j = 0; j < a.length; j++) {
+                projected = normal.x * a[j].x + normal.y * a[j].y;
+                if (isUndefined(minA) || projected < minA) {
+                    minA = projected;
+                }
+                if (isUndefined(maxA) || projected > maxA) {
+                    maxA = projected;
+                }
+            }
+
+            // for each vertex in the second shape, project it onto the line perpendicular to the edge
+            // and keep track of the min and max of these values
+            minB = maxB = undefined;
+            for (let j = 0; j < b.length; j++) {
+                projected = normal.x * b[j].x + normal.y * b[j].y;
+                if (isUndefined(minB) || projected < minB) {
+                    minB = projected;
+                }
+                if (isUndefined(maxB) || projected > maxB) {
+                    maxB = projected;
+                }
+            }
+
+            // if there is no overlap between the projects, the edge we are looking at separates the two
+            // polygons, and we know there is no overlap
+            if (maxA < minB || maxB < minA) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function mapRectToArrayCoordinate(rect) {
+    const { x , y, width, height } = rect.getBoundingClientRect()
+    return [
+        { x, y }, // left-top
+        { x: x + width, y }, // right-top
+        { x: x + width, y: y + height  }, // right-bottom
+        { x, y: y + height  }, // left-bottom
+    ];
 }
 
 /**
